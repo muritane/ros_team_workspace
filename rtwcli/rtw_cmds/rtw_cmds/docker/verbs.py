54b3519e4d7f98a041115fa5fdf926328feff529
Daniel Azanov <sirian.94@mail.ru>
2023-05-05 14:02:45 +0200

    [rtwcli] rtw docker enter: works with workaround

--- rtwcli/rtw_cmds/rtw_cmds/docker/verbs.py
+++ rtwcli/rtw_cmds/rtw_cmds/docker/verbs.py
@@ -12,11 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from rtwcli.verb import VerbExtension
+from rtw_cmds.docker.utils import extract_variables, start_and_connect_user
 
 
 class EnterVerb(VerbExtension):
     """Enter docker container."""
 
     def main(self, *, args):
-        print("Not implemented yet")
+        print("##### enter docker verb begin #####")
+
+        ROS_WS = os.environ.get("ROS_WS", None)
+        if not ROS_WS:
+            print(
+                'It seems ROS_WS was not exported. Did you source your workspace by executing _"<ws_alias>"?'
+            )
+            return
+
+        ros_ws_name = ROS_WS.split("/")[-1]
+        print(f"ROS_WS is exported: {ROS_WS} -> ros_ws_name: {ros_ws_name}")
+
+        print(
+            "RosTeamWS_WS_DOCKER* variables are not exported, therefore getting them from reading '~/.ros_team_ws_rc'"
+        )
+        variables = extract_variables(ros_ws_name, os.path.expanduser("~/.ros_team_ws_rc"))
+        print(f"Read variables: {variables}")
+
+        RosTeamWS_WS_DOCKER_SUPPORT = variables.get("RosTeamWS_WS_DOCKER_SUPPORT", None)
+        if not RosTeamWS_WS_DOCKER_SUPPORT or RosTeamWS_WS_DOCKER_SUPPORT == "false":
+            print(
+                'It seems your current workspace does not support docker. If it should, did you activate it by executing _"<ws_alias>"?'
+            )
+            return
+
+        RosTeamWS_DOCKER_TAG = variables.get("RosTeamWS_DOCKER_TAG", None)
+        print(f"RosTeamWS_DOCKER_TAG: {RosTeamWS_DOCKER_TAG}")
+
+        start_and_connect_user(RosTeamWS_DOCKER_TAG)
+
+        print("##### enter docker verb end #####")
